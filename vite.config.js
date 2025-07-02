99aa5161c9b1540af08d84a41caf252e5133f109
TakshMangaroliya <takshmangaroliya@gmail.com>
2025-07-02 15:25:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 Important for Vercel and relative paths
+  base: './', // <-- This is the fix!
 })
