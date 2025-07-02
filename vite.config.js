d4b554758d44f76fe12a22621dd0b36460b0f5db
Taksh Patel <takshpatel07@gmail.com>
2025-07-02 15:36:05 +0530

    fix: set vite base path for correct deployment

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  
-  build: {
-    rollupOptions: {
-      external: ['gsap'], // 👈 mark gsap as external
-    },
-  },
-});
+  base: './', // ✅ This is the fix!
+})
